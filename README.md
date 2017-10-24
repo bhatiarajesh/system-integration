47b59158dffca65a9cbf4f31333a0b1c5678bbb7
bhatiarajesh <rajesh.bh@gmail.com>
2017-10-23 22:10:17 -0700

    CarND Capstone Project: System Integration
    Initial commit for the CarND Capstone Project: System Integration
    project. Cloned from the Level 5 team master branch
    https://github.com/level5-engineers/system-integration

---
+++ README.md
@@ -0,0 +1,155 @@
+![level5-engineers](https://avatars1.githubusercontent.com/u/31551095?v=4&s=100)
+### level5-engineers
+<b>CarND Capstone Project: System Integration</b><br>
+<em>It’s not about the pieces but how they work together.</em>
+<br>
+
+#### Team
+
+* James Dunn, lead
+* Oleg Leyzerov
+* Aman Agarwal
+* Rajesh Bhatia
+* Yousof Ebneddin
+
+### Objective
+Create code to drive a vehicle in both a Unity-based simulator and a real-world Lincoln MKZ around a closed-circuit test-track. This repository contains all ROS nodes to implement the core functionality of an autonomous vehicle system.
+
+### Implementation Notes
+
+The diagram below illustrates the system architecture. The autonomous vehicle controller is composed of three major units: perception, planning, and control.
+
+![System Architecture](https://raw.githubusercontent.com/level5-engineers/assets/master/images/SystemArchitecture.png)
+Legend: the letters a-k indicate published ROS topics
+
+```
+  a: /camera/image_raw
+  b: /current_pose
+  c: /current_velocity
+  d: /vehicle/dbw_enabled
+  e: /traffic_waypoint
+  f: /base_waypoints
+  g: /final_waypoints
+  h: /twist_cmd
+  i: /vehicle/throttle_cmd
+  j: /vehicle/brake_cmd
+  k: /vehicle/steering_cmd
+```
+
+### Perception
+The traffic light detection and classification node uses a Convolutional Neural Network to classify whole images as either `red` or `none`. The model was trained with several datasets using the transfer learning technique on the MobileNet architecture with the Tensorflow Image Retraining Example (tutorial: https://goo.gl/HgmiVo, code: https://goo.gl/KdVcMi). [more...](#additional-specifications)
+
+### Planning
+The waypoint updater node publishes a queue of `n` waypoints ahead of the vehicle position, each with a target velocity. For the simulator, `n=100` is sufficient. For the site (the real-world test track), we reduce to `n=20`. We dequeue traversed waypoints and enqueue new points, preserving and reusing those in the middle. When a light-state changes, the entire queue is updated. The vehicle stops at the final base waypoint. [more...](#additional-specifications)
+
+### Control
+The drive-by-wire node adjusts throttle and brakes according to the velocity targets published by the waypoint follower (which is informed by the waypoint updater node). If the list of waypoints contains a series of descending velocity targets, the PID velocity controller (in the twist controller component of DBW) will attempt to match the target velocity. [more...](#additional-specifications)
+
+### Operation
+There are three modes in which the controller operates: 
+- site: When at the test site, this mode is launched. This mode can be run simultaneously with a rosbag to test the traffic light classifier. (See below)
+- sitesim: emulates the test site in the simulator at the first traffic light.
+- styx: When using the term3 simulator, this mode is launched. The simulator communicates through server.py and bridge.py
+
+These modes are started by roslaunch. For example, to run the styx (simulator) version we run:
+
+`roslaunch launch/styx.launch`
+
+### Additional Specifications
+
+[Traffic light image classification](https://github.com/level5-engineers/system-integration/wiki/Traffic-Light-Image-Classification)<br>
+[Waypoint updater](https://github.com/level5-engineers/system-integration/wiki/Waypoint-Updater)<br>
+[Drive-by-wire](https://github.com/level5-engineers/system-integration/wiki/Drive-By-Wire)<br>
+
+### References
+
+[MobileNets](https://arxiv.org/abs/1704.04861)<br>
+[Transfer learning](http://ruder.io/transfer-learning/index.html)<br>
+[Pure Pursuit Algorithm](http://ri.cmu.edu/pub_files/pub3/coulter_r_craig_1992_1/coulter_r_craig_1992_1.pdf)<br>
+[Quaternion mathematics](https://web.archive.org/web/20120417090529/http://www.itu.dk/people/erikdam/DOWNLOAD/98-5.pdf)<br>
+[Quaternions online visualization](http://quaternions.online/)<br>
+[PID control](https://udacity-reviews-uploads.s3.amazonaws.com/_attachments/41330/1493863065/pid_control_document.pdf)<br>
+
+---
+
+This is the project repo for the final project of the Udacity Self-Driving Car Nanodegree: Programming a Real Self-Driving Car. For more information about the project, see the project wiki [here](https://github.com/level5-engineers/system-integration/wiki).
+
+### Native Installation
+
+* Be sure that your workstation is running Ubuntu 16.04 Xenial Xerus or Ubuntu 14.04 Trusty Tahir. [Ubuntu downloads can be found here](https://www.ubuntu.com/download/desktop).
+* If using a Virtual Machine to install Ubuntu, use the following configuration as minimum:
+  * 2 CPU
+  * 2 GB system memory
+  * 25 GB of free hard drive space
+
+  The Udacity provided virtual machine has ROS and Dataspeed DBW already installed, so you can skip the next two steps if you are using this.
+
+* Follow these instructions to install ROS
+  * [ROS Kinetic](http://wiki.ros.org/kinetic/Installation/Ubuntu) if you have Ubuntu 16.04.
+  * [ROS Indigo](http://wiki.ros.org/indigo/Installation/Ubuntu) if you have Ubuntu 14.04.
+* [Dataspeed DBW](https://bitbucket.org/DataspeedInc/dbw_mkz_ros)
+  * Use this option to install the SDK on a workstation that already has ROS installed: [One Line SDK Install (binary)](https://bitbucket.org/DataspeedInc/dbw_mkz_ros/src/81e63fcc335d7b64139d7482017d6a97b405e250/ROS_SETUP.md?fileviewer=file-view-default)
+* Download the [Udacity Simulator](https://github.com/udacity/CarND-Capstone/releases/tag/v1.2).
+
+### Docker Installation
+[Install Docker](https://docs.docker.com/engine/installation/)
+
+Build the docker container
+```bash
+docker build . -t capstone
+```
+
+Run the docker file
+```bash
+docker run -p 127.0.0.1:4567:4567 -v $PWD:/capstone -v /tmp/log:/root/.ros/ --rm -it capstone
+```
+
+### Usage
+
+1. Clone the project repository
+```bash
+git clone https://github.com/level5-engineers/system-integration.git
+```
+
+2. Install python dependencies
+```bash
+cd system-integration
+pip install -r requirements.txt
+```
+
+3. Make the controller
+```bash
+cd ros
+catkin_make
+```
+
+4. In a new terminal window, start roscore
+```bash
+roscore
+```
+
+5. Start the simulator, select screen resolution 800x600, click SELECT, uncheck the Manual checkbox. Ideally, run the simulator in the host environment (outside of the virtual machine).
+
+6. In a new terminal window, start the controller
+```bash
+cd system-integration/ros
+source devel/setup.sh
+roslaunch launch/styx.launch
+```
+
+### Real world testing
+1. Download [training bag](https://drive.google.com/file/d/0B2_h37bMVw3iYkdJTlRSUlJIamM/view?usp=sharing) that was recorded on the Udacity self-driving car (a bag demonstraing the correct predictions in autonomous mode can be found [here](https://drive.google.com/open?id=0B2_h37bMVw3iT0ZEdlF4N01QbHc))
+2. Unzip the file
+```bash
+unzip traffic_light_bag_files.zip
+```
+3. Play the bag file
+```bash
+rosbag play -l traffic_light_bag_files/loop_with_traffic_light.bag
+```
+4. Launch your project in site mode
+```bash
+cd system-integration/ros
+roslaunch launch/site.launch
+```
+5. Confirm that traffic light detection works on real life images
---
+++ ros/src/tl_detector/tl_detector.py
@@ -0,0 +1,280 @@
+#!/usr/bin/env python
+import rospy
+import os
+import csv
+from std_msgs.msg import Int32
+from geometry_msgs.msg import PoseStamped, Pose, Quaternion
+from styx_msgs.msg import TrafficLightArray, TrafficLight, Waypoint
+from styx_msgs.msg import Lane
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+from light_classification.tl_classifier import TLClassifier
+import tf
+import cv2
+import yaml
+import math
+import numpy as np
+
+STATE_COUNT_THRESHOLD = 3
+CLASSIFIER_ENABLED = True
+
+from PIL import Image as PIL_Image
+
+class TLDetector(object):
+    def __init__(self):
+        rospy.init_node('tl_detector')
+
+        self.pose             = None
+        self.waypoints        = None
+        self.camera_image     = None
+        self.lights           = []
+        self.bridge           = CvBridge()
+        self.light_classifier = TLClassifier()
+        self.listener         = tf.TransformListener()
+        self.state            = TrafficLight.UNKNOWN
+        self.last_state       = TrafficLight.UNKNOWN
+        self.last_wp          = -1
+        self.state_count      = 0
+        self.camera_image     = None
+        self.seq              = 13332
+        self.count            = 0
+        self.misscount        = 0.
+        self.totcount         = 0.
+        self.sight            = 75.   # cautionary distance
+        self.wpAcquired       = False
+
+        sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
+        sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
+
+        '''
+        /vehicle/traffic_lights provides you with the location of the traffic light in 3D map space and
+        helps you acquire an accurate ground truth data source for the traffic light
+        classifier by sending the current color state of all traffic lights in the
+        simulator. When testing on the vehicle, the color state will not be available. You'll need to
+        rely on the position of the light and the camera image to predict it.
+        '''
+        sub3 = rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb)
+
+        config_string = rospy.get_param("/traffic_light_config")
+        self.config   = yaml.load(config_string)
+
+        self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
+
+        sub6 = rospy.Subscriber('/image_color', Image, self.image_cb)
+        rospy.spin()
+
+    def pose_cb(self, msg):
+        self.pose = msg
+
+    def waypoints_cb(self, msg):
+        if not self.wpAcquired:
+            self.wpAcquired = True
+            self.waypoints = self.filterWaypoints(msg)
+            if self.waypoints[20].twist.twist.linear.x < 5.:
+                self.sight = 20.  # adjust cautionary distance for site test
+            print "cautionary distance", self.sight
+            print self.waypoints[20].twist.twist.linear.x
+
+    def traffic_cb(self, msg):
+        self.lights = msg.lights
+
+    def image_cb(self, msg):
+        """Identifies red lights in the incoming camera image and publishes the index
+            of the waypoint closest to the red light's stop line to /traffic_waypoint
+
+        Args:
+            msg (Image): image from car-mounted camera
+
+        """
+        self.has_image = True
+        self.camera_image = msg
+        light_wp, state = self.process_traffic_lights()
+
+        '''
+        Publish upcoming red lights at camera frequency.
+        Each predicted state has to occur `STATE_COUNT_THRESHOLD` number
+        of times till we start using it. Otherwise the previous stable state is
+        used.
+        '''
+        if self.state != state:
+            self.state_count = 0
+            self.state = state
+        elif self.state_count >= STATE_COUNT_THRESHOLD:
+            self.last_state = self.state
+            light_wp = light_wp if state == TrafficLight.RED or state == TrafficLight.YELLOW else -1
+            self.last_wp = light_wp
+            self.upcoming_red_light_pub.publish(Int32(light_wp))
+        else:
+            self.upcoming_red_light_pub.publish(Int32(self.last_wp))
+        self.state_count += 1
+
+    def distance(self, pos1, pos2):
+        return math.sqrt((pos1.position.x - pos2.position.x)**2 + (pos1.position.y - pos2.position.y)**2)
+
+    def get_closest_waypoint(self, pose):
+        """Identifies the closest path waypoint to the given position
+            https://en.wikipedia.org/wiki/Closest_pair_of_points_problem
+        Args:
+            pose (Pose): position to match a waypoint to
+        
+        Returns:
+            int: index of the closest waypoint in self.waypoints
+
+        """
+        index = -1 #Return if waypoints are empty
+        if self.waypoints is None:
+            return index
+        
+        lowDistance = 0
+        #rospy.loginfo("len(wp): %d", len(self.waypoints))
+        for i in range(len(self.waypoints)):
+            distance = self.distance(pose, self.waypoints[i].pose.pose)
+            
+            if index == -1 or distance < lowDistance:
+                index = i
+                lowDistance = distance
+            
+        return index
+
+    def get_light_state(self, light):
+        """Determines the current color of the traffic light
+
+        Args:
+            light (TrafficLight): light to classify
+
+        Returns:
+            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+
+        """
+        if(not self.has_image):
+            self.prev_light_loc = None
+            return TrafficLight.RED
+
+        # fix camera encoding
+        if hasattr(self.camera_image, 'encoding'):
+            if self.camera_image.encoding == '8UC3':
+                self.camera_image.encoding = "rgb8"
+        else:
+            self.camera_image.encoding = 'rgb8'
+        cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "rgb8")
+
+        #Get classification
+        return self.light_classifier.get_classification(cv_image)
+
+    def get_nearest_stop_line(self, waypoint_start_index):
+        stop_line = None
+        # List of positions that correspond to the line to stop in front of for a given intersection
+        stop_line_positions = self.config['stop_line_positions']
+        last_index = 99999
+        
+        for i in range(0, len(stop_line_positions)):
+            stopline_pose = PoseStamped()
+            stopline_pose.pose.position.x = float(stop_line_positions[i][0])
+            stopline_pose.pose.position.y = float(stop_line_positions[i][1])
+            index = self.get_closest_waypoint(stopline_pose.pose)
+            if index > waypoint_start_index and index < last_index:
+                last_index = index;
+                stop_line = stopline_pose
+        
+        return stop_line, last_index
+
+    def process_traffic_lights(self):
+        """Finds closest visible traffic light, if one exists, and determines its
+            location and color
+        Returns:
+            int: index of waypoint closest to the upcoming stop line for a traffic light (-1 if none exists)
+            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+
+        """
+        if(self.pose):
+            car_position = self.get_closest_waypoint(self.pose.pose)
+            #rospy.loginfo("car_position: %d", car_position)
+            if car_position > 0:
+                stop_pos, stop_waypoint = self.get_nearest_stop_line(car_position)
+                
+                if stop_pos is not None:
+                    #rospy.loginfo("stop_pos x: %.2f, stop_waypoint: %d", stop_pos.pose.position.x, stop_waypoint)
+                    state = TrafficLight.UNKNOWN
+                    
+                    # if the traffic light is within sight, then attempt to classify
+                    if self.distance(self.pose.pose, stop_pos.pose) < self.sight:
+                        
+                        if CLASSIFIER_ENABLED:
+                            state = self.get_light_state(None)
+                            if self.lights is not None:
+                                stateTruth = TrafficLight.UNKNOWN
+                                for light in self.lights:
+                                    # Get the ground truth from /vehicle/traffic_lights
+                                    if self.distance(light.pose.pose, stop_pos.pose) < 30.:
+                                        stateTruth = light.state
+                                        break
+                                if (((stateTruth is 4) or (stateTruth is 2)) and (state is 0)) or (((stateTruth is 0) or (stateTruth is 1)) and (state is 4)) :
+                                    print "Classifier: ", state, " Truth: ", stateTruth
+                                    state = stateTruth
+                                    #self.saveImage(self.camera_image, stateTruth)
+                                    self.misscount += 1.
+                                self.totcount += 1.
+                                print "mismatch%: ", self.misscount / self.totcount
+                                #self.saveImage(self.camera_image, stateTruth)
+                        else:
+                            for light in self.lights:
+                                # This section uses only /vehicle/traffic_lights
+                                if self.distance(light.pose.pose, stop_pos.pose) < 30.:
+                                    state = light.state
+                                    #rospy.loginfo("light state: %d, x: %.2f, y: %.2f", state, self.pose.pose.position.x, self.pose.pose.position.y)
+                                    #rospy.loginfo("dist tl to stop: %.2f", self.distance(light.pose.pose, stop_pos.pose))
+                                    break
+                        return stop_waypoint, state
+        #self.waypoints = None
+        return -1, TrafficLight.UNKNOWN
+
+    def saveImage(self, img, state):
+        #if self.count%2==0:
+            # fix camera encoding
+        if hasattr(img, 'encoding'):
+            if img.encoding == '8UC3':
+                img.encoding = "rgb8"
+        else:
+            img.encoding = 'rgb8'
+        img = self.bridge.imgmsg_to_cv2(img, "rgb8")
+
+        image_data = cv2.resize(img, (224,224))
+        img= PIL_Image.fromarray(image_data, 'RGB')
+        #if state == TrafficLight.RED:
+        #    img.save('/home/student/data/red/out'+str(self.seq).zfill(5)+'.png', 'PNG')
+        #    self.seq += 1
+        if state == TrafficLight.YELLOW:
+            img.save('/home/student/data/yellow/out'+str(self.seq).zfill(5)+'.png', 'PNG')
+            self.seq += 1
+        elif state == TrafficLight.GREEN:
+            img.save('/home/student/data/green/out'+str(self.seq).zfill(5)+'.png', 'PNG')
+            self.seq += 1
+        #else:
+        #    img.save('/home/student/data/unknown/out'+str(self.seq).zfill(5)+'.png', 'PNG')
+        #    self.seq += 1
+
+    def filterWaypoints(self, wp):
+        if wp.waypoints[0].pose.pose.position.x == 10.4062:
+            waypoints = []
+            path = rospy.get_param('~path')
+            if not os.path.isfile(path):
+                return wp.waypoints
+            with open(path) as wfile:
+                reader = csv.DictReader(wfile, ['x','y','z','yaw'])
+                for wp in reader:
+                    p = Waypoint()
+                    p.pose.pose.position.x = float(wp['x'])
+                    p.pose.pose.position.y = float(wp['y'])
+                    p.pose.pose.position.z = float(wp['z'])
+                    q = tf.transformations.quaternion_from_euler(0., 0., float(wp['yaw']))
+                    p.pose.pose.orientation = Quaternion(*q)
+                    p.twist.twist.linear.x = 2.7777778
+                    waypoints.append(p)
+            return waypoints
+        return wp.waypoints
+
+if __name__ == '__main__':
+    try:
+        TLDetector()
+    except rospy.ROSInterruptException:
+        rospy.logerr('Could not start traffic node.')
